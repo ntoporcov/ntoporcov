851b11d257d9187fc033bec6649556c229a67f3d
ntoporcov <42581202+ntoporcov@users.noreply.github.com>
2020-01-01 15:50:45 -0500

    Create epg.php

---
+++ epg.php
@@ -0,0 +1,6 @@
+<?php
+$username=$_GET['username'];
+$password=$_GET['password'];
+$xml = file_get_contents('http://nitro.ltd:25461/xmltv.php?username=WkdQUwvcym&password=JLQp2oor9y');
+echo $xml;
+?>
