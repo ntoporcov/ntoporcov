4a87e2decf574f16dd0a40da49b62d3907306840
ntoporcov <42581202+ntoporcov@users.noreply.github.com>
2020-01-01 15:56:44 -0500

    Update epg.php

--- epg.php
+++ epg.php
@@ -1,6 +1,7 @@
 <?php
 $username=$_GET['username'];
 $password=$_GET['password'];
-$xml = file_get_contents('http://nitro.ltd:25461/xmltv.php?username=WkdQUwvcym&password=JLQp2oor9y');
+$url = htmlspecialchars_decode('http://nitro.ltd:25461/xmltv.php?username=WkdQUwvcym&password=JLQp2oor9y')
+$xml = file_get_contents($url);
 echo $xml;
 ?>
