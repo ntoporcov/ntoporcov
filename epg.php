6f823ec5dfb9d1774f263f6011ca7a2b2cb1f875
ntoporcov <42581202+ntoporcov@users.noreply.github.com>
2020-01-02 07:37:31 -0500

    Delete epg.php

--- epg.php
+++
@@ -1,7 +0,0 @@
-<?php
-$username=$_GET['username'];
-$password=$_GET['password'];
-$url = htmlspecialchars_decode('http://nitro.ltd:25461/xmltv.php?username=WkdQUwvcym&password=JLQp2oor9y')
-$xml = file_get_contents($url);
-echo $xml;
-?>
