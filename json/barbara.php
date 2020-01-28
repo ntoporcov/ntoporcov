8c6237788da226e3362b5aca35dc050e8d8ba540
nicolastoporcov <nic@gleanview.com>
2020-01-27 20:26:52 -0500

    adding marcos

--- json/barbara.php
+++ json/barbara.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+header("Access-Control-Allow-Origin: http://www.marcosbuss.com");
 header('Access-Control-Allow-Credentials: true');
 
 $jsonContent = file_get_contents('./barbara.json');
