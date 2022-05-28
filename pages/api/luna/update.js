d1330adf201be46fdb915dd141e9ec78e2617486
ntoporcov <ntoporcov@me.com>
2022-05-28 15:35:39 -0400

    pushing fix?

--- pages/api/luna/update.js
+++ pages/api/luna/update.js
@@ -10,41 +10,38 @@ export default async function handler(req, res) {
 
   // console.log(name, going, group);
 
-  if (req.method === "POST") {
-    try {
-      const arrayToRemove = going ? "denied" : "accepted";
-      const arrayToAdd = !going ? "denied" : "accepted";
+  const arrayToRemove = going ? "denied" : "accepted";
+  const arrayToAdd = !going ? "denied" : "accepted";
 
-      await firebaseDB
-        .ref()
-        .child("luna")
-        .child(arrayToRemove)
-        .transaction((curr) => {
-          console.log(curr);
-          return curr.filter((nameInArr) => nameInArr !== name);
-        });
+  await firebaseDB
+    .ref()
+    .child("luna")
+    .child(arrayToRemove)
+    .transaction((curr) => {
+      console.log("removing " + name, curr);
+      return [...curr].filter((nameInArr) => nameInArr !== name);
+    });
 
-      await firebaseDB
-        .ref()
-        .child("luna")
-        .child(arrayToAdd)
-        .transaction((curr) => [...curr, name]);
+  await firebaseDB
+    .ref()
+    .child("luna")
+    .child(arrayToAdd)
+    .transaction((curr) => {
+      console.log("confirming  " + name, curr);
+      return [...curr, name];
+    });
 
-      const refObject = firebaseDB.ref().child("luna");
-      const snapshot = await refObject.once("value");
-      const data = snapshot.val();
+  const refObject = firebaseDB.ref().child("luna");
+  const snapshot = await refObject.once("value");
+  const data = snapshot.val();
 
-      const results = data.guests.filter((guestGroup) =>
-        guestGroup.invited
-          .map((name) => name.toLowerCase())
-          .includes(name.toLowerCase())
-      )[0];
+  const results = data.guests.filter((guestGroup) =>
+    guestGroup.invited
+      .map((name) => name.toLowerCase())
+      .includes(name.toLowerCase())
+  )[0];
 
-      res
-        .status(200)
-        .json({ group: results, denied: data.denied, accepted: data.accepted });
-    } catch (e) {
-      res.status(500).json(e);
-    }
-  }
+  res
+    .status(200)
+    .json({ group: results, denied: data.denied, accepted: data.accepted });
 }
