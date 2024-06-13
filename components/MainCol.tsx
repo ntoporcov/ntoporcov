68c0f72dcf2fc815519471ffcacee95191a9c2e5
ntoporcov <ntoporcov@me.com>
2024-06-12 23:39:46 -0400

    just some cleanup

--- components/MainCol.tsx
+++ components/MainCol.tsx
@@ -1,11 +1,14 @@
-import { Container } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 
 const MainCol = (props: PropsWithChildren<{}>) => {
   return (
-    <Container as={"main"} maxW={"1400px"} centerContent pb={"30vh"} px={2}>
+    <main
+      className={
+        "flex max-w-[140px] flex-col items-center justify-center px-5 pb-[30vh]"
+      }
+    >
       {props.children}
-    </Container>
+    </main>
   );
 };
 
